7343cff421497b61ae2ac899f8f5704b40d22f2f
Harrison Stephan <harrisonstephan@Harrisons-iMac.local>
2024-02-28 17:08:02 -0700

    fixed the cache and database

--- client/src-sw.js
+++ client/src-sw.js
@@ -25,18 +25,20 @@ warmStrategyCache({
 });
 
 offlineFallback({
-  pageFallBack: '/index.html'
+  pageFallback: '/index.html'
 })
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute(({ request }) => ["style", "script", "worker"].include(request.destination), new StaleWhileRevalidate({ 
-  cacheName: 'assets-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    })
-  ],
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({ 
+    cacheName: 'assets-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      })
+    ],
 }));
 
